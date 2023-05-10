926fa1c0caec6b608f64e5dadf997bfcea45685e
unknown <keyzafalconx@hotmail.com>
2023-05-10 16:10:24 +0700

    Database Added

--- green_house_demo/settings.py
+++ green_house_demo/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "green_house_demo.wsgi.application"
 
 DATABASES = {
     "default": {
-        #"ENGINE": "django.db.backends.sqlite3",
-        #"NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":  "postgres",
+        "USER":  "postgres",
+        "PASSWORD":  "greenhouse001!?+",
+        "HOST":  "db.plmhcpnlnhgfeydcgijk.supabase.co",
+        "POST":  "5432",
     }
 }
 
