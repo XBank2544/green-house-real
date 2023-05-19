fed09adc587d92f45b49ca5337d122d47cfb8538
unknown <keyzafalconx@hotmail.com>
2023-05-19 15:51:38 +0700

    add Schedule database

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Schedule(models.Model):
+    fan_start = models.DecimalField(max_digits=3, decimal_places=1)
+    fan_stop = models.DecimalField(max_digits=3, decimal_places=1)
+    start_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"Schedule - Fan Start: {self.fan_start}, Fan Stop: {self.fan_stop}, Start At: {self.start_at}, End At: {self.end_at}"
+
 # Create your models here.
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
@@ -8,7 +11,38 @@ def dashboard(request):
     return render (request, 'dashboard.html')
 
 def schedule(request):
-    return render (request, 'schedule.html')
+    if request.method == 'POST':
+        fan_start = request.POST['fan_start']
+        fan_stop = request.POST['fan_stop']
+        start_at = request.POST['start_at']
+        end_at = request.POST['end_at']
+
+        # Save the data to Supabase
+        supabase_url = 'https://your-supabase-url.supabase.co/rest/v1/table-name'
+        supabase_key = 'your-supabase-key'
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {supabase_key}'
+        }
+
+        data = {
+            'fan_start': fan_start,
+            'fan_stop': fan_stop,
+            'start_at': start_at,
+            'end_at': end_at
+        }
+
+        response = requests.post(supabase_url, headers=headers, json=data)
+
+        if response.status_code == 201:
+            # Data saved successfully
+            return redirect('/dashboard')
+        else:
+            # Error occurred while saving data
+            return HttpResponse('Error occurred while saving data')
+    else:
+        return render(request, 'schedule.html')
 
 def login(request):
     return render (request, 'login.html')
