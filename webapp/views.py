cbec67ad012dae936c812fed8b03fcb6d9c48a21
unknown <keyzafalconx@hotmail.com>
2023-05-24 00:48:09 +0700

    Update View.py

--- webapp/views.py
+++ webapp/views.py
@@ -4,22 +4,22 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(requests):
-  return render(requests, "home.html")
+def index(request):
+  return render(request, "home.html")
 
-def dashboard(requests):
-    return render (requests, 'dashboard.html')
+def dashboard(request):
+    return render (request, 'dashboard.html')
 
-def schedule(requests):
-    if requests.method == 'POST':
-        fan_start = requests.POST['fan_start']
-        fan_stop = requests.POST['fan_stop']
-        start_at = requests.POST['start_at']
-        end_at = requests.POST['end_at']
+def schedule(request):
+    if request.method == 'POST':
+        fan_start = request.POST['fan_start']
+        fan_stop = request.POST['fan_stop']
+        start_at = request.POST['start_at']
+        end_at = request.POST['end_at']
 
         # Save the data to Supabase
-        supabase_url = 'https://plmhcpnlnhgfeydcgijk.supabase.co'
-        supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsbWhjcG5sbmhnZmV5ZGNnaWprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3MDg2MjQsImV4cCI6MTk5OTI4NDYyNH0.TSa1If5ES9C-Pr8Ikmeae8yU64IlW0xs5R9KzOcv-kA'
+        supabase_url = 'https://plmhcpnlnhgfeydcgijk.supabase.co/database/tables/webapp_schedule'
+        supabase_key = 'sbp_d93e1cd9e760d209e58103bdfd6b1c0689343aa8'
 
         headers = {
             'Content-Type': 'application/json',
@@ -33,7 +33,7 @@ def schedule(requests):
             'end_at': end_at
         }
 
-        response = requests.post(supabase_url, headers=headers, json=data)
+        response = requests.POST(supabase_url, headers=headers, json=data)
 
         if response.status_code == 201:
             # Data saved successfully
@@ -42,10 +42,10 @@ def schedule(requests):
             # Error occurred while saving data
             return HttpResponse('Error occurred while saving data')
     else:
-        return render(requests, 'schedule.html')
+        return render(request, 'schedule.html')
 
 def login(request):
-    return render (requests, 'login.html')
+    return render (request, 'login.html')
     
 def register(request):
-    return render (requests, 'register.html')
+    return render (request, 'register.html')
