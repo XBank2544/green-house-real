2e4b95a62daaa02aad60b6d5884befea15ca6825
XBank2544 <63918430+XBank2544@users.noreply.github.com>
2023-07-30 08:25:28 +0700

    Update models.py

--- webapp/models.py
+++ webapp/models.py
@@ -8,9 +8,6 @@ class Schedule(models.Model):
 
     def __str__(self):
         return f"Schedule - Fan Start: {self.fan_start}, Fan Stop: {self.fan_stop}, Start At: {self.start_at}, End At: {self.end_at}"
-class Temp(models.Model):
-    temperature = models.DecimalField(max_digits=3, decimal_places=1)
-    def __str__(self):
-        return f"Schedule - Temp: {self.temperature}"
+
 
 # Create your models here.
