874683fa9d109027aa75f5d12d9810edf58f38b7
XBank2544 <63918430+XBank2544@users.noreply.github.com>
2023-07-30 08:30:15 +0700

    Update admin.py

--- webapp/admin.py
+++ webapp/admin.py
@@ -6,5 +6,5 @@ from . import models
 
 @admin.register(models.Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
-    list_display = ('fan_start', 'fan_stop', 'start_at', 'end_at')
+    list_display = ('fan_start', 'fan_stop', 'start_at', 'end_at', 'temp')
 
