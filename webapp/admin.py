288f4007630985539b606bc615863f4e4b00d01e
XBank2544 <63918430+XBank2544@users.noreply.github.com>
2023-07-30 08:25:19 +0700

    Update admin.py

--- webapp/admin.py
+++ webapp/admin.py
@@ -7,6 +7,4 @@ from . import models
 @admin.register(models.Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
     list_display = ('fan_start', 'fan_stop', 'start_at', 'end_at')
-@admin.register(models.Temp)
-class ScheduleAdmin(admin.ModelAdmin):
-    list_display = ('temperature')
+
